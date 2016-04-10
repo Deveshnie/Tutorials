4d9edc194bc579916cada4cfcec1c6ffc7a792e8
Deveshnie <deveshniemudaly@ymail.com>
2016-04-10 19:13:43 +0200

    Added files via upload

---
+++ tut1.asm
@@ -0,0 +1,4 @@
+ls -t #displays last file in directory first
+head -16 nameOfFile #the name of the file must have the file extension included eg. hello_world.py
+man Python (Displays python commands)
+
