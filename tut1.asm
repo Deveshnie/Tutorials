17a4b1df5013d2ac9df1f99a39cc50ca1dd44fba
Deveshnie <deveshniemudaly@ymail.com>
2016-04-13 20:17:01 +0200

    Delete tut1.asm

--- tut1.asm
+++
@@ -1,4 +0,0 @@
-ls -t #displays last file in directory first
-head -16 nameOfFile #the name of the file must have the file extension included eg. hello_world.py
-man Python (Displays python commands)
-
